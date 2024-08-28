2f70fe224922cee454381683917618b15d6bf411
Swati lahane <101633697+SwatiLahane@users.noreply.github.com>
2024-08-28 18:44:38 +0530

    Add files via upload

---
+++ assignment66/EdgeBrowser.java
@@ -0,0 +1,14 @@
+package assignment66;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		
+		EdgeDriver driver = new EdgeDriver(); //to  launch EdgeBrowser
+		
+		
+	}
+
+}
---
+++ assignment66/FireFoxBrowser.java
@@ -0,0 +1,13 @@
+package assignment66;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxBrowser {
+
+	public static void main(String[] args) {
+		
+		FirefoxDriver driver = new FirefoxDriver(); //To launch firefox Driver
+		
+	}
+
+}
